7aa27306dcf3ec72d35f43a4f5eca4d8cd04a555
JonasH <jonas.hoeing@hft-leipzig.de>
2016-04-04 23:17:29 +0200

    Fixes

--- src/JonasPhilMalte/generateSecret.java
+++ src/JonasPhilMalte/generateSecret.java
@@ -17,13 +17,13 @@ public class generateSecret {
 	
 	public SecretKey neuesSecret() throws Exception {
 	
-	KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-	keyGen.init(256);
-	SecretKey secKey = keyGen.generateKey();
-	byte[] key = secKey.getEncoded();
-	String encodedKey = Base64.getEncoder().encodeToString(secKey.getEncoded());
-	System.out.println(encodedKey);
-	System.out.println(key);
-	return secKey;
+		KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+		keyGen.init(256);
+		SecretKey secKey = keyGen.generateKey();
+		byte[] key = secKey.getEncoded();
+		String encodedKey = Base64.getEncoder().encodeToString(secKey.getEncoded());
+		System.out.println(encodedKey);
+		System.out.println(key);
+		return secKey;
 	}
 }
